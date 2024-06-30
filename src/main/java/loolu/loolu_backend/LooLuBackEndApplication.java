4cf32335fb171e68a8eccb02a05777c7abbce47a
Radu Nastas <nastas799@gmail.com>
2024-06-30 15:27:06 +0200

    Initial commit

---
+++ src/main/java/loolu/loolu_backend/LooLuBackEndApplication.java
@@ -0,0 +1,14 @@
+package loolu.loolu_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
+
+@SpringBootApplication
+public class LooLuBackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LooLuBackEndApplication.class, args);
+    }
+
+}
